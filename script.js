871e02f5f19e888da6476b9d681a81e201c3b88c
Vinit Surve <115409878+VinitSurve@users.noreply.github.com>
2024-03-11 11:29:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const fan = document.getElementById('fan');
+  // Your code to manipulate the fan's style
+});
+
+function on() {
+    fan.style.animationDuration = 1 + "s"; 
+}
+function off() {
+    fan.style.animationDuration = 0 + "s"; 
+}
+function fan1() {
+    fan.style.animationDuration = .5 + "s"; 
+}
+function fan2() {
+    fan.style.animationDuration = .30+ "s"; 
+}
+function fan3() {
+    fan.style.animationDuration = .15 + "s"; 
+}
+
